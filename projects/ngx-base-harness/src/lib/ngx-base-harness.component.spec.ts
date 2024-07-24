81a0bad3d053a2a5cfe150ee902c98beb7dcb391
antonborisoff <antonborisoff@mail.ru>
2024-07-24 12:36:58 +0300

    configure eslint (#4)

--- projects/ngx-base-harness/src/lib/ngx-base-harness.component.spec.ts
+++ projects/ngx-base-harness/src/lib/ngx-base-harness.component.spec.ts
@@ -1,23 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing'
 
-import { NgxBaseHarnessComponent } from './ngx-base-harness.component';
+import {
+  NgxBaseHarnessComponent
+} from './ngx-base-harness.component'
 
 describe('NgxBaseHarnessComponent', () => {
-  let component: NgxBaseHarnessComponent;
-  let fixture: ComponentFixture<NgxBaseHarnessComponent>;
+  let component: NgxBaseHarnessComponent
+  let fixture: ComponentFixture<NgxBaseHarnessComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NgxBaseHarnessComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(NgxBaseHarnessComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(NgxBaseHarnessComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- projects/ngx-base-harness/src/lib/ngx-base-harness.service.spec.ts
+++ projects/ngx-base-harness/src/lib/ngx-base-harness.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  TestBed
+} from '@angular/core/testing'
 
-import { NgxBaseHarnessService } from './ngx-base-harness.service';
+import {
+  NgxBaseHarnessService
+} from './ngx-base-harness.service'
 
 describe('NgxBaseHarnessService', () => {
-  let service: NgxBaseHarnessService;
+  let service: NgxBaseHarnessService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NgxBaseHarnessService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(NgxBaseHarnessService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})
